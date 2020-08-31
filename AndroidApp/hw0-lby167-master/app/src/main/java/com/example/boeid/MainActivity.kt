2c80bedee42b4e64cf252f58dfa40658cf493398
lby167 <lby167@gmail.com>
2020-08-30 17:46:42 -0700

    Initial commit

---
+++ AndroidApp/hw0-lby167-master/app/src/main/java/com/example/boeid/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.boeid
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        println("Name:Bo Ying Liu")
+        println("EID:bl27267")
+        println("Email:bl27267@utexas.edu")
+
+        }
+    }
+
+
